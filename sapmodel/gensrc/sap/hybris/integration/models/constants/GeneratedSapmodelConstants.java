b7f17803842bcb4a10cb54ee30d1ada940d86274
Mohammad Nasir Rasul <mohammad.nasir.rasul@sap.com>
2014-02-25 15:05:08 +0100

    Initial Commit, setting up the repo and adding the files generated from the yempty extension using ant extgen
    Change-Id: I1af8667f32db6e6399ca57adf4d35a863f0062ce

---
+++ sapmodel/gensrc/sap/hybris/integration/models/constants/GeneratedSapmodelConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at 24.02.2014 21:57:40                         ---
+ * ----------------------------------------------------------------
+ */
+package sap.hybris.integration.models.constants;
+
+/**
+ * @deprecated use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedSapmodelConstants
+{
+	public static final String EXTENSIONNAME = "sapmodel";
+	
+	protected GeneratedSapmodelConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
---
+++ sapmodel/gensrc/sap/hybris/integration/models/jalo/GeneratedSapmodelManager.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at 24.02.2014 21:57:40                         ---
+ * ----------------------------------------------------------------
+ */
+package sap.hybris.integration.models.jalo;
+
+import de.hybris.platform.jalo.extension.Extension;
+import sap.hybris.integration.models.constants.SapmodelConstants;
+
+/**
+ * Generated class for type <code>SapmodelManager</code>.
+ */
+@SuppressWarnings({"deprecation","unused","cast","PMD"})
+public abstract class GeneratedSapmodelManager extends Extension
+{
+	@Override
+	public String getName()
+	{
+		return SapmodelConstants.EXTENSIONNAME;
+	}
+	
+}
