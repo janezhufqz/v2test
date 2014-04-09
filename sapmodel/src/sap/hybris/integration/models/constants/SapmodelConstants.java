2e419be47964b09553bf4ac9085316a9fb0dbb84
Ali Sidane <ali.sidane@sap.com>
2014-04-09 12:01:00 -0400

    Modified SAP Configuratin
    Change-Id: I6374bfc8aabda93a88c8f0f013f86ef554ae4953

--- sapmodel/src/sap/hybris/integration/models/constants/SapmodelConstants.java
+++ sapmodel/src/sap/hybris/integration/models/constants/SapmodelConstants.java
@@ -25,7 +25,7 @@ public final class SapmodelConstants extends GeneratedSapmodelConstants
 	public static final String CONFIGURATION_PROPERTY_DIVISION = "division";
 	public static final String CONFIGURATION_PROPERTY_CURRENCY = "defaultCurrency";
 
-	public static final String CONFIGURATION_PROPERTY_TRANSACTION_TYPE = "sapcommon_transactionType";
+	public static final String CONFIGURATION_PROPERTY_TRANSACTION_TYPE = "transactionType";
 
 	/** SAP PRICING CONSTANT ATTRIBURES **/
 	public static final String CONFIGURATION_PROPERTY_PRICE_SUBTOTAL_PROCEDURE = "priceSubtotalProcedure";
@@ -33,6 +33,7 @@ public final class SapmodelConstants extends GeneratedSapmodelConstants
 	public static final String CONFIGURATION_PROPERTY_TAXES_SUBTOTAL_PROCEDURE = "taxesSubtotalProcedure";
 	public static final String CONFIGURATION_PROPERTY_DELIVERY_SUBTOTAL_PROCEDURE = "deliverySubtotalProcedure";
 	public static final String CONFIGURATION_PROPERTY_PAYMENT_COST_SUBTOTAL_PROCEDURE = "paymentCostSubtotalProcedure";
+	public static final String CONFIGURATION_PROPERTY_REFERENCE_CUSTOMER = "referenceCustomer";
 
 	private SapmodelConstants()
 	{
