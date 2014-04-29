f1f67645e95d7d23e043a56ecb1d21789ee154cc
Christoph Hinssen <christoph.hinssen@sap.com>
2014-04-29 16:12:58 +0200

    New tests
    Change-Id: I160603fe1a01d9371131ef8b5c024d0a2815d636

--- sapcommonbol/testsrc/de/hybris/platform/sap/sapcommonbol/suites/UnitTestSuite.java
+++ sapcommonbol/testsrc/de/hybris/platform/sap/sapcommonbol/suites/UnitTestSuite.java
@@ -2,6 +2,7 @@ package de.hybris.platform.sap.sapcommonbol.suites;
 
 import de.hybris.platform.sap.sapcommonbol.businesspartner.businessobject.impl.AddressImplTest;
 import de.hybris.platform.sap.sapcommonbol.common.backendobject.impl.ConverterBackendERPCRMTest;
+import de.hybris.platform.sap.sapcommonbol.common.salesarea.businessobject.impl.DistChannelMappingImplTest;
 import de.hybris.platform.sap.sapcommonbol.transaction.util.ConversionHelperTest;
 import de.hybris.platform.sap.sapcommonbol.transaction.util.impl.ConversionToolsTest;
 
@@ -17,22 +18,9 @@ import org.junit.runners.Suite;
 { ConversionHelperTest.class, //
 		ConverterBackendERPCRMTest.class, //
 		ConversionToolsTest.class, //
-		AddressImplTest.class //
+		AddressImplTest.class, //
+		DistChannelMappingImplTest.class, //
 
-// TODO delete this comment once proven test classes not used
-//		test for not not used classes:
-// ConverterFactoryTest.class (from old WEC, not hybris)
-//	PriceInputConverter
-// CommonModuleAccessUnitTest.class,
-// ConverterTests.class
-// ExceptionTests.class,
-// ValidatorTests.class,
-// ComparatorGenericTest.class,
-// PaginatorBeanImplTest.class,
-// RenderContainerBeanImplTest.class,
-// TitleContainerBeanImplTest.class,
-// LoggingToolsTest.class,
-// PairTest.class,
 })
 public class UnitTestSuite
 {
