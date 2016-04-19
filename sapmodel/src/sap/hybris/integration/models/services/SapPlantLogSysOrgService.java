e736e934c502075b2a34352511bbe852062eee4a
i813945 <redwan.wahbeh@sap.com>
2016-04-19 09:20:31 -0400

    Update the read config. service with base store

--- sapmodel/src/sap/hybris/integration/models/services/SapPlantLogSysOrgService.java
+++ sapmodel/src/sap/hybris/integration/models/services/SapPlantLogSysOrgService.java
@@ -3,26 +3,29 @@ package sap.hybris.integration.models.services;
 import de.hybris.platform.sap.sapmodel.model.SAPLogicalSystemModel;
 import de.hybris.platform.sap.sapmodel.model.SAPPlantLogSysOrgModel;
 import de.hybris.platform.sap.sapmodel.model.SAPSalesOrganizationModel;
+import de.hybris.platform.store.BaseStoreModel;
 
 public interface SapPlantLogSysOrgService {
 	
-	
     /**
      * @param plantCode
+     * @param baseStoreModel
      * @return SAP logical system for a given plant
      */
-    public SAPLogicalSystemModel getSapLogicalSystemForPlant(String plantCode);
+    public SAPLogicalSystemModel getSapLogicalSystemForPlant(String plantCode, BaseStoreModel baseStoreModel);
     
     /**
      * @param plantCode
+     * @param baseStoreModel
      * @return SAP sales organization for a given plant
      */
-    public SAPSalesOrganizationModel getSapSalesOrganizationForPlant(String plantCode);
+    public SAPSalesOrganizationModel getSapSalesOrganizationForPlant(String plantCode, BaseStoreModel baseStoreModel);
     
     /**
      * @param plantCode
+     * @param baseStoreModel
      * @return SAP logical system and sales organization for a given plant
      */
-    public SAPPlantLogSysOrgModel getSapPlantLogSysOrgForPlant(String plantCode);
+    public SAPPlantLogSysOrgModel getSapPlantLogSysOrgForPlant(String plantCode, BaseStoreModel baseStoreModel);
     
 }
--- sapmodel/src/sap/hybris/integration/models/services/impl/DefaultSapPlantLogSysOrgService.java
+++ sapmodel/src/sap/hybris/integration/models/services/impl/DefaultSapPlantLogSysOrgService.java
@@ -1,34 +1,28 @@
 package sap.hybris.integration.models.services.impl;
 
+import java.util.Optional;
+import java.util.Set;
+import java.util.function.BiFunction;
+
+import sap.hybris.integration.models.services.SapPlantLogSysOrgService;
 import de.hybris.platform.sap.core.configuration.model.SAPConfigurationModel;
 import de.hybris.platform.sap.sapmodel.exceptions.SAPModelRuntimeException;
 import de.hybris.platform.sap.sapmodel.model.SAPLogicalSystemModel;
 import de.hybris.platform.sap.sapmodel.model.SAPPlantLogSysOrgModel;
 import de.hybris.platform.sap.sapmodel.model.SAPSalesOrganizationModel;
 import de.hybris.platform.store.BaseStoreModel;
-import de.hybris.platform.store.services.BaseStoreService;
-
-import java.util.Optional;
-import java.util.Set;
-import java.util.function.BiFunction;
-
-import org.springframework.beans.factory.annotation.Required;
-
-import sap.hybris.integration.models.services.SapPlantLogSysOrgService;
 
 public class DefaultSapPlantLogSysOrgService implements SapPlantLogSysOrgService {
-
-	private BaseStoreService baseStoreService;
 
 	private final BiFunction<Set<SAPPlantLogSysOrgModel>, String, Optional<SAPPlantLogSysOrgModel>> selectEntry = (
 			sapPlantLogSysOrgSet, plantCode) -> sapPlantLogSysOrgSet.stream().filter(
 			entry -> entry.getPlant().getCode().contentEquals(plantCode)).findFirst();
 
 	@Override
-	public SAPLogicalSystemModel getSapLogicalSystemForPlant(String plantCode) {
+	public SAPLogicalSystemModel getSapLogicalSystemForPlant(String plantCode, BaseStoreModel baseStoreModel) {
 
 		final Optional<SAPPlantLogSysOrgModel> sapPlantLogSysOrg = selectEntry.apply(
-				getCurrentSAPConfiguration().getSapPlantLogSysOrg(), plantCode);
+				getCurrentSAPConfiguration(baseStoreModel).getSapPlantLogSysOrg(), plantCode);
 
 		if (sapPlantLogSysOrg.isPresent()) {
 			return sapPlantLogSysOrg.get().getLogSys();
@@ -38,10 +32,10 @@ public class DefaultSapPlantLogSysOrgService implements SapPlantLogSysOrgService
 	}
 
 	@Override
-	public SAPSalesOrganizationModel getSapSalesOrganizationForPlant(String plantCode) {
+	public SAPSalesOrganizationModel getSapSalesOrganizationForPlant(String plantCode, BaseStoreModel baseStoreModel) {
 		
 		final Optional<SAPPlantLogSysOrgModel> sapPlantLogSysOrg = selectEntry.apply(
-				getCurrentSAPConfiguration().getSapPlantLogSysOrg(), plantCode);
+				getCurrentSAPConfiguration(baseStoreModel).getSapPlantLogSysOrg(), plantCode);
 
 		if (sapPlantLogSysOrg.isPresent()) {
 			return sapPlantLogSysOrg.get().getSalesOrg();
@@ -51,10 +45,10 @@ public class DefaultSapPlantLogSysOrgService implements SapPlantLogSysOrgService
 	}
 
 	@Override
-	public SAPPlantLogSysOrgModel getSapPlantLogSysOrgForPlant(String plantCode) {
+	public SAPPlantLogSysOrgModel getSapPlantLogSysOrgForPlant(String plantCode, BaseStoreModel baseStoreModel) {
 		
 		final Optional<SAPPlantLogSysOrgModel> sapPlantLogSysOrg = selectEntry.apply(
-				getCurrentSAPConfiguration().getSapPlantLogSysOrg(), plantCode);
+				getCurrentSAPConfiguration(baseStoreModel).getSapPlantLogSysOrg(), plantCode);
 
 		if (sapPlantLogSysOrg.isPresent()) {
 			return sapPlantLogSysOrg.get();
@@ -63,31 +57,16 @@ public class DefaultSapPlantLogSysOrgService implements SapPlantLogSysOrgService
 		return null;
 	}
 
-	private SAPConfigurationModel getCurrentSAPConfiguration() {
+	private SAPConfigurationModel getCurrentSAPConfiguration(BaseStoreModel baseStore) {
 
-		BaseStoreModel baseStore = baseStoreService.getCurrentBaseStore();
-
-		if (baseStore == null) {
-			throw new SAPModelRuntimeException("No base store is maintained!");
-		}
 
 		SAPConfigurationModel sapConfiguration = baseStore.getSAPConfiguration();
 
 		if (sapConfiguration == null) {
-			throw new SAPModelRuntimeException(
-					"No SAP configuration is maintained for the base store!");
+			throw new SAPModelRuntimeException("No SAP configuration is maintained for the base store!");
 		}
 
 		return sapConfiguration;
-	}
-
-	public BaseStoreService getBaseStoreService() {
-		return baseStoreService;
-	}
-    
-	@Required
-	public void setBaseStoreService(BaseStoreService baseStoreService) {
-		this.baseStoreService = baseStoreService;
 	}
 
 }
