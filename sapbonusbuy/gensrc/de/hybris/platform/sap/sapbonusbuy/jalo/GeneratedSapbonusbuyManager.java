878b0762d2b5ea0b09596d3a204196c9d8625163
Jenkins @ http://jkmaster.prod.griffin.ecp.ydev.hybris.com:9080/ <cis.jenkins@hybris.com>
2016-07-16 02:59:53 +0200

    Update copyright header and remove author tags - Build #1359

--- sapbonusbuy/gensrc/de/hybris/platform/sap/sapbonusbuy/jalo/GeneratedSapbonusbuyManager.java
+++ sapbonusbuy/gensrc/de/hybris/platform/sap/sapbonusbuy/jalo/GeneratedSapbonusbuyManager.java
@@ -1,19 +1,13 @@
 /*
- * ----------------------------------------------------------------
- * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
- * --- Generated at Jul 15, 2016 1:12:44 PM                     ---
- * ----------------------------------------------------------------
- *  
  * [y] hybris Platform
- *  
- * Copyright (c) 2000-2016 SAP SE
+ *
+ * Copyright (c) 2000-2016 SAP SE or an SAP affiliate company.
  * All rights reserved.
- *  
+ *
  * This software is the confidential and proprietary information of SAP
- * Hybris ("Confidential Information"). You shall not disclose such
- * Confidential Information and shall use it only in accordance with the
- * terms of the license agreement you entered into with SAP Hybris.
- *  
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with SAP.
  */
 package de.hybris.platform.sap.sapbonusbuy.jalo;
 
