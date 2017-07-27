cd5cb1f3d4e1b5a739a118e85e269ad14dcc1bb6
CIS JENKINS <cis.jenkins@hybris.com>
2017-07-27 02:57:15 +0000

    Update copyright header and remove author tags - Build #1410

--- sapmodel/src/de/hybris/platform/sap/sapmodel/authors/SapClassificationAttributeAuthorHelperImpl.java
+++ sapmodel/src/de/hybris/platform/sap/sapmodel/authors/SapClassificationAttributeAuthorHelperImpl.java
@@ -1,6 +1,8 @@
 /*
  * [y] hybris Platform
+ *
  * Copyright (c) 2017 SAP SE or an SAP affiliate company. All rights reserved.
+ *
  * This software is the confidential and proprietary information of SAP
  * ("Confidential Information"). You shall not disclose such Confidential
  * Information and shall use it only in accordance with the terms of the
